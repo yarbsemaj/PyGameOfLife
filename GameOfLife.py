47aa99421890511c5cacbf5f8ef6b83b4a87d368
james <yarbsemaj@gmail.com>
2019-01-31 14:39:23 +0000

    Improved code clarity

--- GameOfLife.py
+++ GameOfLife.py
@@ -15,6 +15,8 @@ rules = np.zeros((2, 8))
 
 def board_setup():
 	"""
+	Setup the initial state of the board
+
 	board[row][column] = 1
 	"""
 	board[6][12] = 1
@@ -38,6 +40,9 @@ def rule_setup():
 
 
 def print_board():
+	"""
+	Prints the current state of the board to standard output
+	"""
 	for row in board:
 		print("|", end='')
 		for cell in row:
@@ -52,6 +57,9 @@ def print_board():
 
 
 def apply_rules():
+	"""
+	Applys the rules to the current game board based on the current state of the adjacency board
+	"""
 	global board
 	for row in range(board_height):
 		for column in range(board_width):
@@ -59,6 +67,12 @@ def apply_rules():
 
 
 def get_cell_content(row,column):
+	"""
+	Gets the contents of a cell, if the cell is out of bounds empty (0) is returned
+	:param row:
+	:param column:
+	:return: The contents of the cell, will be 0 if the cell is out of bounds
+	"""
 	if row < 0 or row >= board_height or column < 0 or column >= board_width:
 		return 0
 	else:
@@ -66,6 +80,9 @@ def get_cell_content(row,column):
 
 
 def calculate_adjacency_board ():
+	"""
+	Calculated the adjacency board with the current state of the game voard
+	"""
 	global adjacency_board
 	for row in range(board_height):
 		for column in range(board_width):
